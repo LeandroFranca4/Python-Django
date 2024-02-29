ee2a438f2a5971f0cdd66dfce010a662e446e670
Leandro França <leandro24798@gmail.com>
2024-02-29 15:25:14 -0300

    save

---
+++ receitas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from receitas.views import home, contato, sobre
+
+urlpatterns = [
+    path('', home), #Home
+    path('sobre/', sobre),
+    path('contato/',contato),
+]
