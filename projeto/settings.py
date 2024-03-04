5aa5a4f9d1a6ff3e8f73cac16ec3b3f171d37dc6
Leandro França <leandro24798@gmail.com>
2024-03-04 11:47:02 -0300

    save

--- projeto/settings.py
+++ projeto/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'receitas' / 'templates_temp'
+            
 
         ],
         'APP_DIRS': True,
