b8fd59f9b8a15b88d69c0b9968449f1ee85c7bb7
Leandro França <leandro24798@gmail.com>
2024-04-05 17:59:42 -0300

    save

--- receitas/views.py
+++ receitas/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 def home(request):
     return render (request, 'receitas/pages/home.html', context={'name': 'Leandro França' })
 
+def receitas(request, id):
+    return render (request, 'receitas/pages/receitas-view.html', context={'name': 'Leandro França' })
+
 
 
 
