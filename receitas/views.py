ee5b430e7aa182f1fcd33797ec231fb333678014
Leandro França <leandro24798@gmail.com>
2024-04-25 16:59:23 -0300

    save

--- receitas/views.py
+++ receitas/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.receitas.factory import make_recipe
 
 # Create your views here.
 
 
 
 def home(request):
-    return render (request, 'receitas/pages/home.html', context={'name': 'Leandro França' })
+    return render (request, 'receitas/pages/home.html', context={'receitas': [make_recipe() for _ in range(10)], })
 
 def receitas(request, id):
-    return render (request, 'receitas/pages/receitas-view.html', context={'name': 'Leandro França' })
+    return render (request, 'receitas/pages/receitas-view.html', context={'receita': make_recipe(), })
 
 
 
